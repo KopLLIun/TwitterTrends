c1ebe29fbbe462d561ce1dba7654a93cdcd7b621
Nikita Radevich <nikita.radevich@mail.ru>
2020-05-01 16:04:53 +0300

    Change Draw Utility for correct display map

--- TwiterTrends/TwiterTrends/Utility/DrawingUtility.cs
+++ TwiterTrends/TwiterTrends/Utility/DrawingUtility.cs
@@ -12,34 +12,41 @@ namespace TwiterTrends.Utility
 {
     static class DrawingUtility
     {
-        public static Polygon DrawMapByCoordinates(JArray coordinates)
+        public static List<Polygon> DrawMapByCoordinates(JArray coordinates)
         {
-            Polygon polygon = new Polygon();
-            PointCollection points = new PointCollection();
+            List<Polygon> polygons = new List<Polygon>();
             if (coordinates.Count == 1)
             {
+                PointCollection points = new PointCollection();
+                Polygon polygon = new Polygon();
                 for (int j = 0; j < coordinates[0].Count(); j++)
                 {
                     //polygon.Points.Add(new Point(Double.Parse(coordinates[0][j][0].ToString()), -Double.Parse(coordinates[0][j][1].ToString())));
-                    points.Add(new Point(Double.Parse(coordinates[0][j][0].ToString()), -Double.Parse(coordinates[0][j][1].ToString())));
+                    points.Add(new Point(Double.Parse(coordinates[0][j][0].ToString()) * 9 + 1400, 
+                        -Double.Parse(coordinates[0][j][1].ToString()) * 9 + 700));
                 }
                 polygon.Points = points;
+                polygons.Add(polygon);
             }
             else
             {
                 for (int i = 0; i < coordinates.Count; i++)
                 {
+                    PointCollection points = new PointCollection();
+                    Polygon polygon = new Polygon();
                     for (int k = 0; k < coordinates[i][0].Count(); k++)
                     {
                         //polygon.Points.Add(
                         //    new Point(Double.Parse(coordinates[i][0][k][0].ToString()), -Double.Parse(coordinates[i][0][k][1].ToString())));
                         points.Add(
-                            new Point(Double.Parse(coordinates[i][0][k][0].ToString()), -Double.Parse(coordinates[i][0][k][1].ToString())));
+                            new Point(Double.Parse(coordinates[i][0][k][0].ToString()) * 9 + 1400, 
+                            -Double.Parse(coordinates[i][0][k][1].ToString()) * 9 + 700));
                     }
+                    polygon.Points = points;
+                    polygons.Add(polygon);
                 }
-                polygon.Points = points;
             }
-            return polygon;
+            return polygons;
         }
     }
 }
