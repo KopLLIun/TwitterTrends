82686099529c7598cc4a004e7cf0b06c34a32d72
Nikita Radevich <nikita.radevich@mail.ru>
2020-05-30 12:34:02 +0300

    Byte array

---
+++ ModelSimpleWayOrganizationMemory/ModelSimpleWayOrganizationMemory/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ОС.Лаба4
+{
+    public partial class Form1 : Form
+    {
+        Timer timer = new Timer();
+        private int memorySize;
+        private int segmentQuantity;
+        private string loadingProcessName;
+        private int loadingProcessSize;
+
+        public bool check(int index, int loadingProcessSize)
+        {
+            if (index + loadingProcessSize > dataGridView1.Rows.Count)
+                return false;
+            else
+            {
+                for (int i = index; i < index + loadingProcessSize; i++)
+                {
+                    if (dataGridView1.Columns[i].Name == "0")
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //create button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            setSegmentSize();
+        }
+
+        //load button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            loadProcessInMemory();
+        }
+
+        //unload button
+        private void button3_Click(object sender, EventArgs e)
+        {
+            unloadProcessFromMemory();
+        }
+
+        //compress button
+        private void button4_Click(object sender, EventArgs e)
+        {
+            compressProcess();
+        }
+
+        private void setSegmentSize()
+        {
+            if (textBox1.Text == String.Empty)
+                MessageBox.Show("Введите размер сегмента памяти.");
+            else if (Convert.ToInt32(textBox1.Text) < 1 || Convert.ToInt32(textBox1.Text) > 4)
+            {
+                MessageBox.Show("Минимальный размер сегмента: 1 Кб\nМаксимальный размер сегмента 4 Кб");
+            }
+            else
+            {
+                dataGridView1.Rows.Clear();
+                //if (correctDataField(textBox1.Text))
+                //{
+                    memorySize = Convert.ToInt32(textBox1.Text);
+                segmentQuantity = Convert.ToInt32(textBox3.Text);
+                dataGridView1.RowCount = memorySize / segmentQuantity;
+                dataGridView1.ColumnCount = segmentQuantity;
+                    for (int i = 0; i < memorySize / segmentQuantity; i++) {
+                    {
+                        for (int j = 0; j < segmentQuantity; j++)
+                        {
+                            //string[] elements = { i.ToString(), "0", (i * 4).ToString(), String.Empty };
+                            dataGridView1.Rows[i].Cells[j].Value = "0";
+                        }
+                    }
+                }
+            }
+        }
+
+        private void loadProcessInMemory()
+        {
+            bool success = false;
+            loadingProcessName = processName.Text;
+            if (correctDataField(processSize.Text))
+            {
+                int convertProcessSize = Convert.ToInt32(processSize.Text);
+                loadingProcessSize = convertProcessSize % 4 == 0 ? convertProcessSize / 4 : convertProcessSize / 4 + 1;
+
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    if (check(i, loadingProcessSize))
+                    {
+                        success = true;
+                        for (int j = i; j < (i + loadingProcessSize); j++)
+                        {
+                            dataGridView1.Columns[0].Name = j.ToString();
+                            dataGridView1.Columns[1].Name = processName.Text;
+                            //listView1.Items[j].SubItems[2].Text = (j * 4).ToString();
+                            //listView1.Items[j].SubItems[3].Text = (j - i).ToString();
+                        }
+                        i = dataGridView1.Rows.Count - 1;
+                    }
+                }
+            }
+            if (!success)
+                MessageBox.Show("Для процесса нет места.");
+        }
+
+        private void unloadProcessFromMemory()
+        {
+            //if (textBox2.Text == String.Empty)
+            //    MessageBox.Show("Введите название процесса.");
+            //else
+            //{
+            //    for (int i = 0; i < listView1.Items.Count; i++)
+            //    {
+            //        if (listView1.Items[i].SubItems[1].Text == textBox2.Text)
+            //        {
+            //            listView1.Items[i].SubItems[0].Text = i.ToString();
+            //            listView1.Items[i].SubItems[1].Text = "0";
+            //            //listView1.Items[i].SubItems[2].Text = (i * 4).ToString();
+            //            //listView1.Items[i].SubItems[3].Text = String.Empty;
+            //        }
+            //    }
+            //}
+        }
+
+        private void compressProcess()
+        {
+            //int i = 0;
+            //while (i < listView1.Items.Count)
+            //{
+            //    if (listView1.Items[i].SubItems[1].Text == "0")
+            //    {
+            //        // Сдвигаем все строки после i на одну вверх
+            //        for (int j = i; j < listView1.Items.Count - 1; j++)
+            //        {
+            //            listView1.Items[j].SubItems[1].Text = listView1.Items[j + 1].SubItems[1].Text;
+            //            listView1.Items[j].SubItems[2].Text = listView1.Items[j + 1].SubItems[2].Text;
+            //            listView1.Items[j].SubItems[3].Text = listView1.Items[j + 1].SubItems[3].Text;
+            //        }
+            //        // Очищаем освободившуюся строку
+            //        int last = listView1.Items.Count - 1;
+            //        listView1.Items[last].SubItems[1].Text = "0";
+            //        listView1.Items[last].SubItems[2].Text = "";
+            //        listView1.Items[last].SubItems[3].Text = "";
+            //    }
+            //    i++;
+            //}
+        }
+
+        private bool correctDataField(string data)
+        {
+            if (String.IsNullOrWhiteSpace(data))
+            {
+                MessageBox.Show("Ошибка ввода данных");
+                return false;
+            }
+            return true;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != Convert.ToChar(8))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox1_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != Convert.ToChar(8))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void processSize_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != Convert.ToChar(8))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ModelSimpleWayOrganizationMemory/ModelSimpleWayOrganizationMemory/Form1.designer.cs
@@ -0,0 +1,276 @@
+﻿namespace ОС.Лаба4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.processName = new System.Windows.Forms.TextBox();
+            this.processSize = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 208);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Битовая матрица";
+            // 
+            // processName
+            // 
+            this.processName.Location = new System.Drawing.Point(8, 23);
+            this.processName.Margin = new System.Windows.Forms.Padding(4);
+            this.processName.Name = "processName";
+            this.processName.Size = new System.Drawing.Size(87, 22);
+            this.processName.TabIndex = 2;
+            // 
+            // processSize
+            // 
+            this.processSize.Location = new System.Drawing.Point(8, 55);
+            this.processSize.Margin = new System.Windows.Forms.Padding(4);
+            this.processSize.Name = "processSize";
+            this.processSize.Size = new System.Drawing.Size(87, 22);
+            this.processSize.TabIndex = 3;
+            this.processSize.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.processSize_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(104, 27);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Название процесса";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(104, 59);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Размер процесса";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 15);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(157, 18);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(230, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Размер оперативной памяти (Кб)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(416, 18);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 28);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Создать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(256, 27);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 53);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Загрузка";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.processName);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.processSize);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.ForeColor = System.Drawing.Color.Black;
+            this.groupBox1.Location = new System.Drawing.Point(16, 93);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(357, 110);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Загрузка процесса";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.ForeColor = System.Drawing.Color.Black;
+            this.groupBox2.Location = new System.Drawing.Point(416, 54);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(161, 110);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Выгрузка процесса";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(8, 71);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(140, 28);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Выгрузка";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 20);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 17);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Название процесса";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(8, 39);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(139, 22);
+            this.textBox2.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 233);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(582, 150);
+            this.dataGridView1.TabIndex = 14;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(16, 44);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(132, 22);
+            this.textBox3.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(160, 48);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 17);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Размер блока";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 430);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox processName;
+        private System.Windows.Forms.TextBox processSize;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
