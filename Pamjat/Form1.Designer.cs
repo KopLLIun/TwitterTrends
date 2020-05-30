ae61e2978a7d58609579d1376c31d2786778792b
Nikita Radevich <nikita.radevich@mail.ru>
2020-05-30 12:33:00 +0300

    Memory Control

---
+++ Pamjat/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace ОС.Лаба4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.processName = new System.Windows.Forms.TextBox();
+            this.processSize = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(16, 187);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(345, 153);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Номер сегмента";
+            this.columnHeader1.Width = 101;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Заполненность";
+            this.columnHeader2.Width = 107;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 167);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Оперативная память";
+            // 
+            // processName
+            // 
+            this.processName.Location = new System.Drawing.Point(8, 23);
+            this.processName.Margin = new System.Windows.Forms.Padding(4);
+            this.processName.Name = "processName";
+            this.processName.Size = new System.Drawing.Size(87, 22);
+            this.processName.TabIndex = 2;
+            // 
+            // processSize
+            // 
+            this.processSize.Location = new System.Drawing.Point(8, 55);
+            this.processSize.Margin = new System.Windows.Forms.Padding(4);
+            this.processSize.Name = "processSize";
+            this.processSize.Size = new System.Drawing.Size(87, 22);
+            this.processSize.TabIndex = 3;
+            this.processSize.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.processSize_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(104, 27);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Название процесса";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(104, 59);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Размер процесса";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 15);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(104, 22);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(136, 18);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(230, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Размер оперативной памяти (Кб)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(381, 18);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 28);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Создать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(256, 27);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 53);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Загрузка";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.processName);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.processSize);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.ForeColor = System.Drawing.Color.Black;
+            this.groupBox1.Location = new System.Drawing.Point(16, 54);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(357, 110);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Загрузка процесса";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Button3);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.ForeColor = System.Drawing.Color.Black;
+            this.groupBox2.Location = new System.Drawing.Point(381, 54);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(161, 110);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Выгрузка процесса";
+            // 
+            // Button3
+            // 
+            this.Button3.Location = new System.Drawing.Point(8, 71);
+            this.Button3.Margin = new System.Windows.Forms.Padding(4);
+            this.Button3.Name = "Button3";
+            this.Button3.Size = new System.Drawing.Size(140, 28);
+            this.Button3.TabIndex = 2;
+            this.Button3.Text = "Выгрузка";
+            this.Button3.UseVisualStyleBackColor = true;
+            this.Button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 20);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 17);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Название процесса";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(8, 39);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(139, 22);
+            this.textBox2.TabIndex = 0;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(8, 39);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 63);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Уплотнить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Location = new System.Drawing.Point(381, 171);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Size = new System.Drawing.Size(123, 110);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Уплотнение памяти";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 354);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox processName;
+        private System.Windows.Forms.TextBox processSize;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button Button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ Pamjat/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ОС.Лаба4
+{
+    public partial class Form1 : Form
+    {
+        Timer timer = new Timer();
+        private int memorySize;
+        private int segmentQuantity;
+        private string loadingProcessName;
+        private int loadingProcessSize;
+
+        public bool check(int index, int loadingProcessSize)
+        {
+            if (index + loadingProcessSize > listView1.Items.Count)
+                return false;
+            else
+            {
+                for (int i = index; i < index + loadingProcessSize; i++)
+                {
+                    if (listView1.Items[i].SubItems[1].Text != "0")
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //create button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            setMemorySize();
+        }
+
+        //load button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            loadProcessInMemory();
+        }
+
+        //unload buttno
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            unloadProcessFromMemory();
+        }
+
+        //compress button
+        private void button4_Click(object sender, EventArgs e)
+        {
+            compressProcess();
+        }
+
+        private void setMemorySize()
+        {
+            if (textBox1.Text == String.Empty)
+                MessageBox.Show("Введите размер оперативной памяти.");
+            else if (Convert.ToInt32(textBox1.Text) < 16 || Convert.ToInt32(textBox1.Text) > 512)
+            {
+                MessageBox.Show("Минимальный объем памяти: 16 Кб\nМаксимальный объем паяти 512 Кб");
+            }
+            else
+            {
+                listView1.Items.Clear();
+                if (correctDataField(textBox1.Text))
+                {
+                    memorySize = Convert.ToInt32(textBox1.Text);
+                    segmentQuantity = memorySize / 4;
+                    for (int i = 0; i < segmentQuantity; i++)
+                    {
+                        string[] elements = { i.ToString(), "0", (i * 4).ToString(), String.Empty };
+                        listView1.Items.Add(new ListViewItem(elements));
+                    }
+                }
+            }
+        }
+
+        private void loadProcessInMemory()
+        {
+            bool success = false;
+            loadingProcessName = processName.Text;
+            if (correctDataField(processSize.Text))
+            {
+                int convertProcessSize = Convert.ToInt32(processSize.Text);
+                loadingProcessSize = convertProcessSize % 4 == 0 ? convertProcessSize / 4 : convertProcessSize / 4 + 1;
+
+                for (int i = 0; i < listView1.Items.Count; i++)
+                {
+                    if (check(i, loadingProcessSize))
+                    {
+                        success = true;
+                        for (int j = i; j < (i + loadingProcessSize); j++)
+                        {
+                            listView1.Items[j].SubItems[0].Text = j.ToString();
+                            listView1.Items[j].SubItems[1].Text = processName.Text;
+                            //listView1.Items[j].SubItems[2].Text = (j * 4).ToString();
+                            //listView1.Items[j].SubItems[3].Text = (j - i).ToString();
+                        }
+                        i = listView1.Items.Count - 1;
+                    }
+                }
+            }
+            if (!success)
+                MessageBox.Show("Для процесса нет места.");
+        }
+
+        private void unloadProcessFromMemory()
+        {
+            if (textBox2.Text == String.Empty)
+                MessageBox.Show("Введите название процесса.");
+            else
+            {
+                for (int i = 0; i < listView1.Items.Count; i++)
+                {
+                    if (listView1.Items[i].SubItems[1].Text == textBox2.Text)
+                    {
+                        listView1.Items[i].SubItems[0].Text = i.ToString();
+                        listView1.Items[i].SubItems[1].Text = "0";
+                        //listView1.Items[i].SubItems[2].Text = (i * 4).ToString();
+                        //listView1.Items[i].SubItems[3].Text = String.Empty;
+                    }
+                }
+            }
+        }
+
+        private void compressProcess()
+        {
+            int i = 0;
+            while (i < listView1.Items.Count)
+            {
+                if (listView1.Items[i].SubItems[1].Text == "0")
+                {
+                    // Сдвигаем все строки после i на одну вверх
+                    for (int j = i; j < listView1.Items.Count - 1; j++)
+                    {
+                        listView1.Items[j].SubItems[1].Text = listView1.Items[j + 1].SubItems[1].Text;
+                        listView1.Items[j].SubItems[2].Text = listView1.Items[j + 1].SubItems[2].Text;
+                        listView1.Items[j].SubItems[3].Text = listView1.Items[j + 1].SubItems[3].Text;
+                    }
+                    // Очищаем освободившуюся строку
+                    int last = listView1.Items.Count - 1;
+                    listView1.Items[last].SubItems[1].Text = "0";
+                    listView1.Items[last].SubItems[2].Text = "";
+                    listView1.Items[last].SubItems[3].Text = "";
+                }
+                i++;
+            }
+        }
+
+        private bool correctDataField(string data)
+        {
+            if (String.IsNullOrWhiteSpace(data))
+            {
+                MessageBox.Show("Ошибка ввода данных");
+                return false;
+            }
+            return true;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != Convert.ToChar(8))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox1_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != Convert.ToChar(8))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void processSize_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != Convert.ToChar(8))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
